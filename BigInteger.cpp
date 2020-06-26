d16c6884aaa484b6c0367c42ca27319ed4fb9115
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-06-27 00:04:19 +0300

    last constructor and assignment operations

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -67,4 +67,71 @@ BN::BN(uint64_t x) : data((sizeof(uint64_t) + bz - 1) / bz)
     norm(data);
 }
 
+BN::BN(const string &str, RadixBI radix)
+{
+    if(radix == RadixBI::dec)
+    {
+        BN bn(1,0);
+        for (auto i : str) {
+            if (i < '0' || i > '9')
+                continue;
+            bn.mulbaseappr(10);
+            bn += BN(i - '0');
+        }
+        data = move(bn.data);
+        return;
+    }
 
+    // radix: hex
+    size_t length = str.size();
+    data.resize((length + bz * 2 - 1) / (bz * 2));
+
+    size_t index = data.size() - 1;
+    size_t shift = bz * 2 - (data.size() * bz * 2 - length);
+
+    for (size_t i = 0; i < length; ++i) {
+        bt d;
+        if (str[i] >= '0' && str[i] <= '9')
+            d = str[i] - '0';
+        else if (str[i] >= 'A' && str[i] <= 'F')
+            d = str[i] - 'A' + 10;
+        else if (str[i] >= 'a' && str[i] <= 'f')
+            d = str[i] - 'a' + 10;
+        else
+            throw invalid_argument(string("BN constructor: invalid char '") + str[i] + "' in HEX string");
+        data[index] = (data[index] << 4) | d;
+        --shift;
+        if (shift == 0) {
+            shift = bz * 2;
+            --index;
+        }
+    }
+    norm(data);
+}
+
+// Swap operation
+
+void BN::swap(BN &bn) noexcept
+{
+    data.swap(bn.data);
+}
+
+// Assignment operations
+
+BN & BN::operator = (const BN& bn)
+{
+    if (this == &bn)
+        return *this;
+        
+    data = bn.data;
+    return *this;
+}
+
+BN & BN::operator = (BN&& bn) noexcept
+{
+    if (this == &bn)
+        return *this;
+
+    data = move(bn.data);
+    return *this;
+}
